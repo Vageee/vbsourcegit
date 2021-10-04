b6f800b4ae1c98932fcc00b72516468a9f048c97
Vageesh <vageeshbeliya4638@gmail.com>
2021-10-04 18:38:28 +0530

    Updated

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ mydb = mysql.connector.connect(host="localhost", user="vageesh", passwd="1964", 
 
 mycursor = mydb.cursor()
 
+#Insertion to DB
+sqlform = "Insert into movielist1(name,actor,actress,director,yearofrelease) values(%s,%s,%s,%s,%s)"
+
+movielist1 = [("vajrakaya","Shivraj","Nabha","Harsha","2016"),("KGF","yash","Srinidhi","Prashanth","2018")]
+
+mycursor.executemany(sqlform,movielist1)
+
+mydb.commit()
+
 mycursor.execute("select * from movielist1")
 
 user = mycursor.fetchall()
